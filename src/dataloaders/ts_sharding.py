4f5e76500353b7a8c2bbab9893ab10cec30fa125
Willa Potosnak <wpotosna@dgx1.auton.cs.cmu.edu>
2026-03-14 18:12:43 -0400

    reorganize tests

--- src/dataloaders/ts_sharding.py
+++ src/dataloaders/ts_sharding.py
@@ -75,7 +75,7 @@ import torch
 from torch import Tensor
 from torch.utils.data import Dataset
 
-from dataloaders.ts_dataloader import SeriesMetadata, _pivot_to_arrays
+from tsfm_base.src.dataloaders._ts_dataloader import SeriesMetadata, _pivot_to_arrays
 
 
 def write_sharded_dataset(
